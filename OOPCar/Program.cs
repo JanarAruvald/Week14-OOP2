ff00ab5ce52f52ef111eb155f63f5a0fe89a3a57
Opilane TTHK <opilane@tthk.ee>
2021-12-01 13:04:07 +0200

    OOPCar

---
+++ OOPCar/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace OOPCar
+{
+    class Program
+    {
+        class Car
+        {
+            string MarkName;
+            string ModelName;
+            string registrationNumber;
+            string color;
+            int Odometer;
+            int Fuel;
+
+            public Car(string _name,string _model,string _RegistrationNumber,string _color)
+            {
+                MarkName = _name;
+                ModelName = _model;
+                registrationNumber = _RegistrationNumber;
+                color = _color;
+                Odometer = 0;
+                Fuel = 60;
+
+                Console.WriteLine($"Your {color} {MarkName} {ModelName} {registrationNumber} has been arrived!");
+            }
+            public string Model
+            {
+                get { return ModelName; }
+            }
+            public string Mark
+            {
+                get { return MarkName; }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public string RegitrationNumber
+            {
+                get { return registrationNumber; }
+            }
+            public int Odo
+            {
+                get { return Odometer; }
+            }
+            public int FuelTank
+            {
+                get { return Fuel; }
+            }
+            public void Drive()
+            {
+                Fuel -= 5;
+                Odometer += 100;
+                Console.WriteLine("Wroom Wroom!");
+            }
+
+            public void ShowCarInfo()
+            {
+                Console.WriteLine($"Mark Name: {MarkName}.");
+                Console.WriteLine($"Model Name: {ModelName}.");
+                Console.WriteLine($"Registration Number: {registrationNumber}.");
+                Console.WriteLine($"Color: {Color}.");
+                Console.WriteLine($"Odometer: {Odometer}.");
+                Console.WriteLine($"Fuel Tank: {Fuel}.");
+                GC.Collect();
+            }
+  
+        }
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("BMW", "M4", "HUT722", "Yellow");
+
+            while(myCar.FuelTank > 0)
+            {
+                myCar.Drive();
+            }
+
+            myCar.ShowCarInfo();
+        }
+    }
+}
---
+++ ObjectsFromFile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace ObjectsFromFile
+{
+    class Program
+    {
+        class Movie
+        {
+            public string title;
+            public string rating;
+            public string year;
+
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                year = _year;
+                rating = _rating;
+
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            foreach(string movie in GetDataFromFile())
+            {
+                string[] tempArray = movie.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2]);
+                Console.WriteLine($"Title: {newMovie.title}, Rating: {newMovie.rating}, Year: {newMovie.year}");
+            }
+
+            
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\opilane\samples\movies.txt";
+            string[] datafromfile = File.ReadAllLines(filePath);
+            return datafromfile;
+        }
+        public static void DisplayArrayElements(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+    }
+}
